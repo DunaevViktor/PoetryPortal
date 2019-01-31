87b0dfb1fafd0a1b703c15bc91b7f9f084d9c986
Viktor Dunaev <rulit.ivan@yandex.ru>
2019-01-31 18:36:46 +0300

    start task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Block = function () {
+    return <div>Ky!</div>
+}
+
+/*class Block extends React.Component {
+    render() {
+     return <div>Ky!</div>
+    }
+}*/
+
+ReactDOM.render(
+    Block,
+    document.querySelector('#root')
+)
