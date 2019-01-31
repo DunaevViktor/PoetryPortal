2de79bfad10710f0fb332a38df1291a69404b348
Viktor Dunaev <rulit.ivan@yandex.ru>
2019-01-31 22:35:31 +0300

    commit for checking

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+
+/* Task 2 */
+/* Возможно конструкция не работала т.к. первый параметр 
+в ReactDOM.render это элемент, а наш элемент функция,
+изменив Block на Block() у меня заработало */
 
 const Block = function () {
-    return <div>Ky!</div>
+    return <div> Ky! </div>
 }
 
-/*class Block extends React.Component {
-    render() {
-     return <div>Ky!</div>
-    }
-}*/
+ReactDOM.render(
+    Block(),
+    document.querySelector('#root')
+)
+
+/* Task 3 */
+
+const styleObj = {
+    background: 'red',
+    color:'black', 
+    fontFamily:'Lobster'
+};
+
+const myClassName = "biba";
+
+const Hack = function () {
+    const a = "Hackerman";
+    return a;
+}
 
 ReactDOM.render(
-    Block,
-    document.querySelector('#root')
+    <div className={myClassName}>
+        <label htmlFor="input" type="text"/>
+        <button style={styleObj}>Click me!</button>
+        <button style={styleObj}>{Block()}</button>
+        <button style={styleObj}>{Hack()}</button>
+    </div>,
+    document.querySelector('#test')
 )
