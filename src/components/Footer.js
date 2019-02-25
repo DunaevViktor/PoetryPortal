d59db2d3bbbf51d98b1262e6f56dbde1882ee3a7
Viktor Dunaev <rulit.ivan@yandex.ru>
2019-02-25 18:23:19 +0300

    fix bugs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,47 @@
 import React from "react";
 import "./Footer.css";
 
-const Links = {
-  twitter: "https://twitter.com/viktor_dunaev",
-  facebook: "https://www.facebook.com/geras.lord",
-  vk: "https://vk.com/viva_belarus_mc",
-  instagram: "https://www.instagram.com/viktordunaev"
-};
+const Links = [
+  {
+    name: "twitter",
+    link: "https://twitter.com/viktor_dunaev",
+    style: "fab fa-twitter-square fa-3x"
+  },
+  {
+    name: "facebook",
+    link: "https://www.facebook.com/geras.lord",
+    style: "fab fa-facebook-square fa-3x"
+  },
+  {
+    name: "vk",
+    link: "https://vk.com/viva_belarus_mc",
+    style: "fab fa-vk fa-3x"
+  },
+  {
+    name: "instagram",
+    link: "https://www.instagram.com/viktordunaev",
+    style: "fab fa-instagram fa-3x"
+  }
+];
 
-const Footer = function() {
-  return (
-    <footer id="poetry-portal-footer">
-      <a href={Links.facebook} className="footer-button-float">
-        <i className="fab fa-facebook-square fa-3x" />
-      </a>
+class Footer extends React.Component {
+  render() {
+    const list = [];
+    for (let i = 0; i < Links.length; i++) {
+      list.push(
+        <a
+          href={Links[i].link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-button-float"
+        >
+          <i className={Links[i].style} />
+        </a>
+      );
+    }
 
-      <a href={Links.twitter} className="footer-button-float">
-        <i className="fab fa-twitter-square fa-3x" />
-      </a>
-
-      <a href={Links.vk} className="footer-button-float">
-        <i class="fab fa-vk fa-3x" />
-      </a>
-
-      <a href={Links.instagram} className="footer-button-float">
-        <i class="fab fa-instagram fa-3x" />
-      </a>
-    </footer>
-  );
-};
+    return <div>${list}</div>;
+  }
+}
 
 export default Footer;
